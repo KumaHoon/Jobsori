bfbb34825f46ea9e1380dfdfb64ed9b767731195
KumaHoon <52783067+KumaHoon@users.noreply.github.com>
2022-08-13 13:21:57 +0900

    Login page 구현

--- App.js
+++ App.js
@@ -6,106 +6,46 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StyleSheet, Text, Image, View, Button} from 'react-native';
+import LoginScreen, {SocialButton} from "react-native-login-screen";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <LoginScreen
+          logoImageSource={require('./assets/bank.png')}
+          onLoginPress={() => {}}
+          onSignupPress={() => {}}
+          onEmailChange={(email: string) => {}}
+          onPasswordChange={(password: string) => {}}>
+          <SocialButton text="Continue with Google" onPress={() => {}} />
+          <SocialButton
+            text="Continue with Facebook"
+            imageSource={require('./assets/social/facebook.png')}
+            onPress={() => {}}
+          />
+          <SocialButton
+            text="Continue with Twitter"
+            imageSource={require('./assets/social/twitter.png')}
+            onPress={() => {}}
+          />
+        </LoginScreen>
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  hello: {
+    fontSize: 30,
+    color: 'red',
   },
 });
 
